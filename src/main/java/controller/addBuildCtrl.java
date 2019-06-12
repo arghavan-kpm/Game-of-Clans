4fe5c3c948e08a701299eabd11384b8e0156f46b
Kosar Pourahmadi <arghavan.kpm@gmail.com>
2019-06-12 18:39:19 +0430

    servlet for a making new buildings in clan

---
+++ src/main/java/controller/addBuildCtrl.java
@@ -0,0 +1,42 @@
+package controller;
+
+import domain.Connect2DB;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+public class addBuildCtrl extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Connect2DB c2d = new Connect2DB();
+        try {
+            Connection con = c2d.connect2DB();
+            String user = request.getParameter("user");
+            String clanId = request.getParameter("clanId");
+            String type = request.getParameter("type");
+
+            CallableStatement cs = null;
+
+
+            cs = con.prepareCall("{ call addBuilding(?,?)}");
+            cs.setString(1,type);
+            cs.setInt(2,Integer.parseInt(clanId));
+            cs.execute();
+
+            request.setAttribute("user" , user);
+            request.setAttribute("clanId" , clanId);
+            request.getRequestDispatcher("MainMenu.jsp").forward(request, response);
+
+
+        } catch (Exception e) {
+            System.out.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+    }
+}
